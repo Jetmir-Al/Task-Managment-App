b92d48d9e40421a813aea77e8a1c3f4b0b1cfce0
Jetmir-Al <jetmiralshiqi0@gmail.com>
2025-12-30 22:06:02 +0100

    login css done

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,15 +1,29 @@
-import { useState } from "react";
+// import { useState } from "react";
+import "./pageStyles/login.css";
+
 
 const Login = () => {
-    const [name, setName] = useState<string>();
+    // const [name, setName] = useState<string>();
     return (
-        <div className="login-Container">
+        <div className="login-container">
             <div className="loginForm">
                 <h3>Login</h3>
                 <label>
-                    Username:
-                    <input type="text" />
+                    Email: <br /> <br />
+                    <input type="email"
+                        placeholder="Enter your email here!"
+                    />
                 </label>
+                <label>
+                    Password: <br /> <br />
+                    <input type="password"
+                        placeholder="Enter your password here!"
+                    />
+                </label>
+
+                <div>
+                    Already have an account? <a href="">Sign up</a>
+                </div>
             </div>
         </div>
     );
